9e83aff2489613f463bfeded819d9f3dad851ad9
urma <syl000126@naver.com>
2022-09-29 04:23:24 +0900

    spring security 커스텀

---
+++ Chaplin/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.demo.config;
+
+import com.example.demo.config.auth.PrincipalDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PrincipalDetailsService principalDetailsService;
+    // 암호화 방식 빈(Bean) 생성
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(this.principalDetailsService);
+
+        return daoAuthenticationProvider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            // user 페이지 설정
+            .antMatchers("/mypage/**").authenticated() // 로그인 필요
+            // 기타 url은 모두 허용
+            .anyRequest().permitAll()
+            .and()
+            // 로그인 페이지 사용
+            .formLogin();
+
+        http.csrf().disable();
+//                .loginPage("/login") // 로그인 페이지 경로 설정
+//                .defaultSuccessUrl("/"); // 로그인 성공 후 기본적으로 리다이렉트되는 경로
+    }
+
+}
