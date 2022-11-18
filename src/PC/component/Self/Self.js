13c6ed4484981669e9b2462d865393489b4f36eb
김다인 <dbslzhs1218@naver.com>
2022-11-19 02:10:11 +0900

    본인인증 기능 구현 완료, 회원가입 수정 및 기능 구현

--- src/PC/component/Self/Self.js
+++ src/PC/component/Self/Self.js
@@ -5,33 +5,93 @@ import axios from 'axios';
 
 function Self() {
 
+    const [name, setName] = useState('');
     const [phoneNumber, setPhone] = useState('');
     const [certification, setCertification] = useState('');
     const [num, setNum] = useState('');
 
-    const onChange = (e) => {
-        setPhone(e.target.value);
+    const[isName, setIsName] = useState(true);
+    const[isPhone, setIsPhone] = useState(true);
+    const[isCerti, setIsCerti] = useState(true);
+
+    //console.log(num);
+    //console.log(isPhone);
+    console.log(isCerti);
+    //console.log(certification == num);
+
+    const onNameHandler = (e) => {
+        setName(e.target.value);
+        if(!e.target.value) {
+            setIsName(true);
+        }else{
+            setIsName(false);
+        }
     }
-    const phoneSubmit = (e) => {
+
+    const onPhoneHandler = (e) => {
+        setPhone(e.target.value);
+        if(!e.target.value) {
+            setIsPhone(true);
+        }else{
+            setIsPhone(false);
+        }
+ 
+    }
+
+    const onCertiHandler = (e) => {
+        setCertification(e.target.value);
+        if(!e.target.value) {
+            setIsCerti(true);
+        }else {
+            setIsCerti(false);
+        }
+    }
+
+    const phoneSubmit = () => {
         //console.log(phoneNumber);
-        axios.get('/sms', phoneNumber).then((response) => {
+        axios.get('/sms', {
+            params: {
+                'phoneNumber':phoneNumber
+            }
+        }).then((response) => {
                 if (response.data) {
-                  console.log(response.data);
+                  //console.log(response.data);
                   setNum(response.data);
                 } else {
                   alert("failed to");
                 }
-              });
+              }).catch(function (error) {
+                alert('error');
+            });
     }
 
-    const certificationSubmit = (e) => {
+
+
+    function certificationSubmit() {
         //console.log(phoneNumber);
         
             if (certification == num) {
                 alert('본인인증에 성공하였습니다');
             } else {
-                alert("failed to");
+                alert("인증번호를 다시 입력해주세요.");
+                setCertification('');
+                setIsCerti(true);
             }
+    }
+
+    const onSubmitHandler = () => {
+        if (isName) {
+            alert('이름을 입력하세요.');
+        }
+        else if (isPhone) {
+            alert('전화번호를 입력하세요.');
+        }
+        else if (isCerti) {
+            alert('인증번호를 입력하세요.');
+        }
+        else if (!num) {
+            alert("인증번호를 확인해주세요");
+        }
     }
 
     return(
@@ -42,24 +102,25 @@ function Self() {
         <p>본인인증을 위해 정보를 입력해주시길 바랍니다.</p>
         <div class="name">
         <div className="name_text">이름 <div className='red_star'>*</div></div>
-            <input type="name" className="input_name" placeholder="이름을 입력하세요."/>
+            <input type="name" className="input_name" placeholder="이름을 입력하세요." onChange={onNameHandler}/>
         </div>
 
         <div class="phone">
         <div className="phone_text">전화번호 <div className='red_star'>*</div></div>
-            <input type="name" className="input_phone" placeholder="전화번호를 입력하세요." onChange={onChange} value={phoneNumber}/>
+            <input type="name" className="input_phone" placeholder="전화번호를 입력하세요." onChange={onPhoneHandler}/>
             <button className='phone_button' onClick={phoneSubmit}>인증요청</button>
         </div>
 
         <div class="num">
         <div className="num_text">인증번호 <div className='red_star'>*</div></div>
-            <input type="name" className="input_num" placeholder="인증번호를 입력하세요." onChange={(e)=>{setCertification(e.target.value)}} value={certification}/>
-            <div className='num_button' onClick={certificationSubmit}>
-            <Link to="#"><input type="submit" value="인증확인" /></Link>
-            </div>
+            <input type="name" className="input_num" placeholder="인증번호를 입력하세요." onChange={onCertiHandler} value={certification}/>
+            <button className='num_button' onClick={certificationSubmit}>인증확인</button>
         </div>
         <div className="self_next">
-        <Link to="/signup"><input type="submit" value="다음" /></Link>
+            {isName == false && isPhone == false && isCerti == false && num && certification == num ?
+        (<Link to="/signup"><input type="submit" value="다음" /></Link>):(
+            <input type="submit" value="다음" onClick={onSubmitHandler}/> 
+        )}
         </div>
         </div>
 
--- src/PC/component/Signup/Signup.js
+++ src/PC/component/Signup/Signup.js
@@ -1,66 +1,228 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Signup.css";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 function Signup() {
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
+    const [confirmPw, setConfirmPw] = useState('');
+    const [emailFirst, setEmailFirst] = useState('');
+    const [emailSelect, setEmailSelect] = useState('');
+    const [name, setName] = useState('');
+    const [gender, setGender] = useState('');
+
+    const email = emailFirst != '' &&  emailSelect != '' ? (emailFirst + "@" + emailSelect) : ('');
+
+    const [isId, setIsId] = useState(false);
+    const [isPw, setIsPw] = useState(false);
+    const [isConfirmPw, setIsConfirmPw] = useState(false);
+    const [isName, setIsName] = useState(true);
+    const [isGender, setIsGender] = useState(true);
+
+    const [id2, setId2] = useState('');
+
+    console.log(id);
+    console.log(pw);
+    console.log(confirmPw);
+    console.log(email);
+    console.log(name);
+    console.log(gender);
+    console.log(isId);
+    console.log(isPw);
+    console.log(isConfirmPw);
+    console.log(email != '');
+    console.log(isName);
+    console.log(isGender);
+
+
+    const onIdHandler = (e) => {
+        let idCheck = /^[A-Za-z0-9]{6,16}$/; 
+        if (idCheck.test(e.target.value) || !e.target.value) {
+            setIsId(false);
+            setId(e.target.value);
+        }else{
+            setIsId(true);
+            //console.log('error');
+        }
+        
+    }
+
+    const onPwHandler = (e) => {
+        let pwCheck = /^[A-Za-z0-9!@#$%^&*()_+-=~`:;'",./?<>]{8,16}$/;
+        if (pwCheck.test(e.target.value) || !e.target.value) {
+            setIsPw(false);
+            setPw(e.target.value);
+        }else{
+            //console.log('error');
+            setIsPw(true);
+        }
+        
+    }
+
+    const onConfirmPwHandler = (e) => {
+        let pwCheck = /^[A-Za-z0-9!@#$%^&*()_+-=~`:;'",./?<>]{8,16}$/;
+        if (pwCheck.test(e.target.value) || !e.target.value) {
+            setIsConfirmPw(false);
+            setConfirmPw(e.target.value);
+        }else{
+            //console.log('error');
+            setIsConfirmPw(true);
+        }
+        
+    }
+
+    const onEmailFirstHandler = (e) => {
+        setEmailFirst(e.target.value);
+    }
+
+    const onEmailSelectHandler = (e) => {
+        setEmailSelect(e.target.value);
+    }
+
+
+    const onNameHandler = (e) => {
+        setName(e.target.value);
+        if(!e.target.value) {
+            setIsName(true);
+        }else{
+            setIsName(false);
+        }
+    }
+
+
+    const onGenderHandler = (e) => {
+        setGender(e.target.value);
+        if(!e.target.value) {
+            setIsGender(true);
+        }else{
+            setIsGender(false);
+        }
+    }
+
+    const onSubmitHandler = () => {
+        if(id == '') {
+            return alert("아이디를 입력하세요");
+        }
+        else if(pw == '') {
+            return alert("비밀번호를 입력하세요.");
+        }
+        else if(confirmPw  == '') {
+            return alert("비밀번호 확인을 입력하세요.");
+        }
+        else if (pw !== confirmPw) {
+            return alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+        }
+        else if (email == '') {
+            return alert("이메일을 입력하세요.");
+        }
+        else if(name == '') {
+            return alert("이름을 입력하세요");
+        }
+        else if(gender == '') {
+            return alert("성별을 선택하세요");
+        }
+    }
+
+    function signPost() {
+        axios.post('/usr', {
+            usr_id: id,
+            usr_pw: pw,
+            usr_nm: name,
+            usr_gn: gender,
+            usr_em: email,
+            usr_role: 'ROLE_USER'
+        }).then(function (response) {
+            console.log(response.data);
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    function duplicateId () {
+        axios.get('/usr/{id}', {
+            params:{
+                'id' : id
+            }
+        }).then((response)=>{
+            if (response.data) {
+                console.log('response.data')
+            } else {
+                
+              }
+            }).catch(function (error) {
+              alert('error');
+          });
+    }
+
+    
+
     return(
         <div className='signups'>
         <div className='signup_left'></div>
 
         <div className='signup'>
-            <div class="signup_id">
-                <div className='sub_title'>회원정보 입력</div>
+        <div className='sub_title'>회원정보 입력</div>
+        <div class="signup_id">
+                
             <div className="id_text">아이디 <div className='red_star'>*</div></div>
-                <input type="id" className='input_id' placeholder='아이디를 입력하세요.'/>
+                <input type="id" className='input_id' placeholder='영문 또는 숫자 6~16자로 입력해주세요.' onChange={onIdHandler}/>
+                <button className='button_id' onClick={duplicateId}>중복확인</button>
+                {isId == true? (<div>영문 또는 숫자 6~16자로 입력해주세요.</div>) : (null)}
             </div>
 
             <div class="signup_pw">
             <div className="pw_text">비밀번호 <div className='red_star'>*</div></div>
-                <input type="password" className="input_pw" placeholder="비밀번호를 입력하세요."/>
+                <input type="password" className="input_pw" placeholder=" 영문, 숫자, 특수문자를 이용하여 8~16자로 입력해주세요. " 
+                onChange={onPwHandler} />
+                {isPw == true? (<div>영문, 숫자, 특수문자를 이용하여 8~16자로 입력해주세요.</div>) : (null)}
+
             </div>
 
             <div class="signp_confirm">
             <div className="confirm_text">비밀번호 확인 <div className='red_star'>*</div></div> 
-                <input type="password_confirm" className="input_pw_confirm" />
+                <input type="password" className="input_pw_confirm" onChange={onConfirmPwHandler} />
+                {isConfirmPw == true? (<div>영문, 숫자, 특수문자를 이용하여 8~16자로 입력해주세요.</div>) : (null)}
+
             </div>   
 
             <div class="email">
             <div className="email_text"> 이메일 <div className='red_star'>*</div></div>
             <div className='email_input'>
-                <input className="box" type="text"/>
+                <input className="box" type="text" onChange={onEmailFirstHandler} />
                 @ 
-                <select className="select_box">
+                <select className="select_box" onChange={onEmailSelectHandler}>
+                <option value="선택하세요">선택하세요.</option>
                 <option value="naver.com">naver.com</option>
                 <option value="google.com">google.com</option>
                 <option value="hanmail.net">hanmail.net</option>
                 <option value="nate.com">nate.com</option>
                 <option value="kakao.com">kakao.com</option>
-                <option value="선택하세요">선택하세요.</option>
                 </select>
                 </div>
             </div>
 
             <div class="name">
             <div className="name_text">이름 <div className='red_star'>*</div></div>
-                <input type="name" className="input_name" />
+                <input type="name" className="input_name" onChange={onNameHandler}/>
             </div>
             
-            <div class="birth">
-            <div className="birth_text">생년월일 <div className='red_star'>*</div></div>
-                <input type="name" className="input_birth" />
-            </div>
 
             <div class="gender">
             <div className="gender_text">성별 <div className='red_star'>*</div></div>
-                    <input className="gender_woman" type="radio"/> 여자
-                    <input className="gender_man" type="radio"  /> 남자
-
+                <input className="gender_woman" type="radio" name='gender' value='여자' onClick={onGenderHandler}/> 여자
+                <input className="gender_man" type="radio" name='gender' value='남자' onClick={onGenderHandler}/> 남자
             </div>
 
             <div class="signup_next ">
-            <Link to="/login"><input type="submit" value="확인" /></Link>
-            </div>
+            {isId == false && isPw == false && (pw == confirmPw) == true && (email != '') == true && isName == false && isGender == false ?
+            (<Link to="/login"><input type="submit" value="확인" onClick={signPost}/></Link>) : (
+                <input type="submit" value="확인" onClick={onSubmitHandler} />
+            )}</div>
+            
+            
             <div className='already_login'><Link to="/login">이미 채플린의 회원이신가요?</Link></div>
+            
         </div>
 
         <div className='signup_right'></div>
